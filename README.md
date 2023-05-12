b1ad280e62bd0fc6e6db2dc252296e178b025ccb
damian <dsamsoniuk@protonmail.com>
2023-05-12 12:35:28 +0200

    app add

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# app-hybryda
-Aplikacja desktop
+# App Hybryd
+
+Korzystałem z:
+
+* nodejs 16
+* npm 8.19
+
+## Debug-owanie, czyli odpal lokalnie aplikacje oraz pokazuj bledy w konsoli
+```
+npm run electron:serve
+``` 
+
+## Stworz build dla systemu linux czyli gotowa aplikacje
+```
+npm run electron:build -- --linux
+npm run electron:build -- --windows
+```
+
+## Windows jak uruchomic aplikacje
+```
+dist/win-unpacked/vue-desktop.exe
+```
+
+## Linux jak uruchomic aplikacje
+```
+./dist_electron/linux-unpacked/vue-desktop
+```
+
+
+
+
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+      <h1>Akcja lokalna: {{ count }}</h1>
+      <button @click="count++">Dodaj</button>
+
+      <p>Wiadomosc z pamieci: {{ message }}</p>
+      <input v-model="message" placeholder="edit me" />
+
+      <button @click="zapisz">Zapisz wiadomosc do pamiecie</button>
+      <br/>---Po ponownym uruchomieniu aplikacji wiadomosc nadal bedzie widoczna
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      count: 0,
+      message: '-'
+    }
+  },
+  props: {
+    msg: String,
+  },
+  mounted() {
+    if (localStorage.name) {
+      this.message = localStorage.name;
+    }
+  },
+  methods: {
+    zapisz() {
+      // `this` inside methods points to the current active instance
+      localStorage.name = this.message;
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
