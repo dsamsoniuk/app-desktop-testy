b104311ab4d286a0780775751f117ca1cb9d2642
damian <dsamsoniuk@protonmail.com>
2023-05-15 11:36:00 +0200

    testy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,21 +11,36 @@
       <br/>---Po ponownym uruchomieniu aplikacji wiadomosc nadal bedzie widoczna
 
 
+      <hr/>
+      lista zwierzat:
+      <ul id="example-1">
+        <li v-for="item in animals" :key="item._id">
+          {{ item.text }}
+        </li>
+      </ul>
 
-      <button @click="download" class="btn btn-primary">Zapisz wiadomosc do pamiecie</button>
+
+      <button @click="fetchCatFacts" class="btn btn-primary">Pobierz axios</button>
+      <button @click="deleteAnimal" class="btn btn-danger">Usun</button>
 
   </div>
 </template>
 
 <script>
-import Car from "../models/Car"
+// import Car from "../models/Car"
+import LocalStoreHelper from "../helpers/LocalStoreHelper"
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
+
   data() {
     return {
       count: 0,
       message: '-',
-      infos:['ss']
+      infos:['ss'],
+      animals: [],
+      localStoreHelper: new LocalStoreHelper(),
     }
   },
   props: {
@@ -36,29 +51,41 @@ export default {
       this.message = localStorage.name;
     }
 
-    localStorage.infos = ['aaa','sssss'];
-    let car = new Car()
-    car.FirstName = 'volkswagen';
-
-    localStorage.setItem('Car', JSON.stringify([
-      car, car
-    ]));
+    if (localStorage.animals) {
+      this.animals =  this.localStoreHelper.getItemFromString('animals');
+    }
+    console.log(localStorage)
 
   },
   methods: {
-    download() {
-
+    deleteAnimal() {
+      this.animals.pop();
+      localStorage.setItem('animals', JSON.stringify(this.animals) )
     },
+    async fetchCatFacts() {
+      try {
+        let res =  await axios.get('https://cat-fact.herokuapp.com/facts/random?amount=5');
+        console.log(res)
+        if (res.status == 200) {
+          this.localStoreHelper.setItemToString('animals', res.data)
+          this.animals = this.localStoreHelper.getItemFromString('animals')
+        }
+      }
+      catch(err) {
+        console.log(err)
+      }
+
+      },
     zapisz() {
       // `this` inside methods points to the current active instance
       localStorage.name = this.message;
       // localStorage.infos = [2,3,5,6,7];
       // let io = localStorage.infos
       // localStorage.infos = io.push('sss')
-      let carList = JSON.parse(localStorage.getItem('Car'))
-      for (let i in carList) {
-        console.log(new Car(carList[i]))
-      }
+      // let carList = JSON.parse(localStorage.getItem('Car'))
+      // for (let i in carList) {
+      //   console.log(new Car(carList[i]))
+      // }
 // console.log(JSON.parse('Car'))
     }
   }
